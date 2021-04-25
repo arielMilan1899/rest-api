3a22ed1e1217081f879e4e184105ff86c325a574
abarrios <radke1899@gmail.com>
2021-04-25 10:23:32 -0400

    Adding index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+//index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//Import routes
+const apiRoutes = require("./src/routes");
+const { PORT } = require('./config');
+
+const app = express();
+
+// Welcome message
+app.get('/', (req, res) => res.send('Welcome to Express'));
+
+//Use cors
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Use API routes in the App
+app.use('/api', apiRoutes);
+
+// Launch app to the specified port
+app.listen(PORT, function () {
+    console.log("Running FirstRest on Port " + PORT);
+});
