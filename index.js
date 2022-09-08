0d4d4908f98d0271200df3f54dc230802b0bb3ac
abarrios <radke1899@gmail.com>
2022-09-08 15:04:39 +0300

    Preparing server

--- index.js
+++ index.js
@@ -2,39 +2,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const axios = require('axios');
 
-async function useProxy(request, method) {
-  const data = JSON.stringify({
-    "collection": "InvestmentVehicles",
-    "database": "Dictionary",
-    "dataSource": "Cluster0"
-  });
-
-  const config = {
-    method: 'post',
-    url: 'https://data.mongodb-api.com/app/data-vazmi/endpoint/data/v1/action/find',
+async function useProxy(request) {
+  const req = new Request('https://data.mongodb-api.com/app/data-vazmi/endpoint/data/v1/action/find', {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Request-Headers': '*',
       'api-key': 'SMkerQXsfb8nr1wRDpIXuhGIYCJjliCyuMAyS9xP2LNZrS0GGe6e7nYXAfYJdwnv',
     },
-    data: data
-  };
+    body: {
+      "collection": "InvestmentVehicles",
+      "database": "Dictionary",
+      "dataSource": "Cluster0"
+    },
+  })
+  // Send the request on to the gcloud function.
+  const response = await fetch(req)
 
-  try {
-    const {data} = await axios(config)
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+  return response
 }
 
 const app = express();
 
 app.get('/', async (req, res) => {
-  const data = await useProxy(req, 'POST')
-  res.status(200).json(data)
+  return await useProxy(req, 'POST')
 });
 
 //Use cors
